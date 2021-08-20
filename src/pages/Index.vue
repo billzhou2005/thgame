0059938662b78214144b80743c20e2dada9203ea
billzhou2005 <billzhou2005@gmail.com>
2021-08-20 11:49:30 -0400

    create register menu

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,39 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <q-div class="q-gutter-y-md column" style="max-width: 300px">
+      <br>
+      <br>
+      <q-input v-model="email" filled type="email" hint="Email" />
+      <q-input filled square v-model="username" hint="Please input user name" :dense="dense" />
+      <q-input v-model="password" filled :type="isPwd ? 'password' : 'text'" hint="Please input your password">
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+      </q-input>
+      <br>
+      <br>
+      <q-btn color="secondary" icon-right="done" label="Register" />
+
+  </q-div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
-  name: 'PageIndex'
+  name: 'PageIndex',
+  setup () {
+    return {
+      email: ref(''),
+      password: ref(''),
+      isPwd: ref(true),
+      username: ref(''),
+      ph: ref(''),
+      dense: ref(false)
+    }
+  }
 })
 </script>
